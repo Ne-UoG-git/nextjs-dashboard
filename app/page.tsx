6ea6a4f30d98418f298671bdb3d28f11bcd9f6d0
Nega <negaagmas3217@gmail.com>
2025-03-01 01:05:09 +1100

    streaming

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
-import AcmeLogo from '@/app/ui/acme-logo';
-import { ArrowRightIcon } from '@heroicons/react/24/outline';
-import Link from 'next/link';
-import styles from '@/app/ui/home.module.css'; //global styling (Style module)
-import Image from 'next/image';
+import AcmeLogo from "@/app/ui/acme-logo";
+import { ArrowRightIcon } from "@heroicons/react/24/outline";
+import Link from "next/link";
+import styles from "@/app/ui/home.module.css"; //global styling (Style module)
+import Image from "next/image";
 //import { lusitana } from '@/app/ui/fonts';
 
 export default function Page() {
@@ -17,14 +17,14 @@ export default function Page() {
           <p
           // className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}
           >
-            <strong>Welcome to Acme.</strong> This is the example for the{' '}
+            <strong>Welcome to Acme.</strong> This is the example for the{" "}
             <a href="https://nextjs.org/learn/" className="text-blue-500">
               Next.js Learn Course
             </a>
             , brought to you by Vercel.
           </p>
           <Link
-            href="/login"
+            href="app/ui/login"
             className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
           >
             <span>Log in</span> <ArrowRightIcon className="w-5 md:w-6" />
